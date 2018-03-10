bbd436166832ce2e9d05fa321fdf9243abd2eb11
HXhester <hanxu_hester@163.com>
2018-03-10 19:44:20 +0100

    Test with is mutual gaze function, working!

--- Assets/Scripts/EyeRaycaster.cs
+++ Assets/Scripts/EyeRaycaster.cs
@@ -10,11 +10,16 @@ public class EyeRaycaster : MonoBehaviour {
     //public float m_RayLength;
     //public RaycastHit eyeHit;
     public KeyCode ToggleVisualizerKey = KeyCode.F12;
+    public GameObject eyeOrig;
 
     private bool rayToggler;
+    
 
     void Update()
     {
+        if(eyeOrig == null)
+            return;
+
         //var ray = new Ray(transform.position, SubscribingToHMDGazeData.SubscribingInstance.GazeDirection);
         //RaycastHit info;
         //Physics.Raycast(ray, out info, m_RayLength);
@@ -26,7 +31,7 @@ public class EyeRaycaster : MonoBehaviour {
 
         if (rayToggler)
         {
-            Debug.DrawRay(transform.position, SubscribingToHMDGazeData.SubscribingInstance.GazeDirection, Color.blue);
+            Debug.DrawRay(eyeOrig.transform.position, eyeOrig.transform.forward*10f, Color.blue);
         }        
     }
 
