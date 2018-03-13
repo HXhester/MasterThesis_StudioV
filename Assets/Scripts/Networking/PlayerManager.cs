6258e0d1c85b6639a0073981a06741d7b9e7206a
HXhester <hanxu_hester@163.com>
2018-03-13 19:33:46 +0100

    fix for only master client

--- Assets/Scripts/Networking/PlayerManager.cs
+++ Assets/Scripts/Networking/PlayerManager.cs
@@ -120,6 +120,9 @@ public class PlayerManager : Photon.PunBehaviour
 
     void Update()
     {
+        if (!PhotonNetwork.isMasterClient || eyes.Length != 2)
+            return;
+
         for (int i = 0; i < eyes.Length; i++)
         {
             eyeOrigs[i].transform.rotation = eyes[i].transform.rotation;
