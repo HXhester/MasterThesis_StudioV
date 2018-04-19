4a58831b615aca7a4276844030e758aa4b104e12
HXhester <hanxu_hester@163.com>
2018-04-19 22:57:19 +0200

    Remove gaze receiver

--- Assets/Scripts/Networking/PlayerManager.cs
+++ Assets/Scripts/Networking/PlayerManager.cs
@@ -7,7 +7,6 @@ using HTC.UnityPlugin.StereoRendering;
 public class PlayerManager : Photon.PunBehaviour {
     private SceneManager_Game _sceneManager;
     private Transform expressionController;
-    private GameObject GazeReceiver;
     private RectTransform GazeIndicator;
     private RectTransform CanvasRect;
     private GameObject localeyeorig;
@@ -83,7 +82,6 @@ public class PlayerManager : Photon.PunBehaviour {
 
     void Start() {
         if (photonView.isMine) {
-            GazeReceiver = GameObject.Instantiate(Resources.Load("Receiver", typeof(GameObject))) as GameObject;
             GazeIndicator = GameObject.Find("GazeIndicator").GetComponent<RectTransform>();
             CanvasRect = GameObject.Find("FaceTrackUI").GetComponent<RectTransform>();
         }
@@ -92,13 +90,6 @@ public class PlayerManager : Photon.PunBehaviour {
     void Update() {
         if (!photonView.isMine || !GameManager.Instance.UsingVR)
             return;
-        if (GameManager.Instance.remoteEye != null){
-            GazeReceiver.transform.position = new Vector3(GameManager.Instance.remoteEye.transform.position.x, 0, 0);
-        }
-        if (GameManager.Instance.localEye.transform.position.x > GazeReceiver.transform.position.x)
-        {
-            GazeReceiver.transform.localEulerAngles = new Vector3(0, 180f, 0);
-        }
 
         RaycastHit hit;
         Vector3 pointOnScreen;
